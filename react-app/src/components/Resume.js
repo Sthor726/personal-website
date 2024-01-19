557e29eb1eb441138f9ef938518f0b58bf1aa9a6
Samuel Thorson <sthor726@gmail.com>
2024-01-19 11:08:39 -0600

    Finished Resume page

--- react-app/src/components/Resume.js
+++ react-app/src/components/Resume.js
@@ -9,22 +9,16 @@ function Resume() {
       <h2 class="font-sans text-6xl font-bold pt-32 ml-32 mr-auto w-fit p-1 h-auto text-transparent bg-clip-text bg-gradient-to-r from-green-700 via-emerald-600 to-teal-500">
         Resume
       </h2>
-      <div class="flex">
-        <img
-          class="pt-32 max-w-4xl h-auto"
-          alt="My Resume"
-          src="/images/resume.png"
-        />
-        <a
-          class="h-16 rounded-full py-3 px-6"
-          href="/images/resume.png"
-          download
-        >
-          <button class=" border-2 border-black text-black h-16 rounded-full py-3 px-6 m-auto mt-32 group-hover:bg-black group-hover:text-slate-100 transition-all ease-out duration-300">
-            Download
-          </button>
-        </a>
-      </div>
+      <img
+        class="pt-32 max-w-4xl h-auto ml-auto mr-auto"
+        alt="My Resume"
+        src="/images/resume.png"
+      />
+      <a class="py-3 px-6" href="/images/resume.png" download>
+        <button class=" text-black py-3 px-6 m-auto mb-32 hover:underline">
+          Download
+        </button>
+      </a>
     </div>
   );
 }
