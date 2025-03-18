072ed813005f24090b560f119c5f5e577024a004
Samuel Thorson <sthor726@gmail.com>
2025-03-18 11:54:33 -0500

    projects page styling

--- react-app/src/components/Projects.js
+++ react-app/src/components/Projects.js
@@ -8,21 +8,26 @@ function Projects() {
         className="mt-6 w-screen h-auto shadow-inner shadow-gray-400 bg-white pb-32"
       >
         <h2 className="font-sans text-6xl font-bold pt-32 md:ml-32 md:mr-16 w-fit p-1 h-auto text-transparent bg-clip-text bg-gradient-to-r from-green-700 via-emerald-600 to-teal-500">
-          Raspberry Pi Google Calendar Display
+          Projects
         </h2>
-        <div className="text-left md:ml-36 md:mr-16 mt-32 text-gray-700">
+        <hr className="border-1 border-gray-400 md:ml-32 md:mr-16 mt-8"></hr>
+        <h3 className="font-sans font-bold text-4xl md:ml-32 md:mr-16 mt-16 w-fit text-transparent bg-clip-text bg-gradient-to-r from-green-700 via-emerald-600 to-teal-500">
+          Raspberry Pi Smart Clock
+        </h3>
+        <div className="text-left md:ml-36 md:mr-16 mt-16 text-gray-700">
           <p className="text-base">
-            This project leverages a Raspberry Pi and a 2-inch LCD screen to create a display that shows upcoming events from my Google Calendar. The display shows the current time, the day of the week, and details about the next scheduled event, including its time, date, and title.
+            This project leverages a Raspberry Pi and a 2-inch LCD screen to create a display that shows upcoming events from my Google Calendar. The display shows the current time, the day of the week, and details about scheduled events, including its time, date, and title.
           </p>
           <h3 className="font-bold text-xl mt-6">Key Features</h3>
           <ul className="pl-8 text-base">
             <li><strong>Google Calendar Integration</strong>: The project uses the Google Calendar API to fetch upcoming events directly from my Google Calendar. By using a service account and OAuth2 authentication,<br></br>
                  it ensures secure and seamless access to event data.</li>
-            <li><strong>Dynamic Event Display</strong>: The display dynamically updates every 5 seconds to show the current time, the day of the week, and the details of the next event. 
-                If there are no upcoming events, a fallback message ("No upcoming events.") is shown.</li>
-            <li><strong>Error Handling</strong>: The application includes robust error handling to deal with any issues that may arise when parsing event times or interacting with the Google Calendar API. 
-            If the event data is in an incorrect format or there is no connection to the API, appropriate fallback messages are displayed.</li>
-          </ul>
+            <li><strong>Dynamic Event Display</strong>: The display dynamically updates every 5 seconds to show the current time, the day of the week, and the details of upcoming events. 
+                If there are no upcoming events, a fallback message "No upcoming events." is shown.</li>
+            <li><strong>3D Printed Case</strong>: The Raspberry Pi and LCD module are housed in a 3D printed case meant to resemble
+                an old-fashioned TV.</li>
+            
+            </ul>
           <h3 className="font-bold text-xl mt-6">Technical Details</h3>
           <ul className="pl-8 text-base">
             <li><strong>Raspberry Pi</strong>: The project runs on a Raspberry Pi, utilizing the GPIO pins to interface with the 2-inch LCD screen.</li>
