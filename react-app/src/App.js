a4fe9d2d9e9a550cddfa0faae73bc60f26eac633
Samuel Thorson <sthor726@gmail.com>
2025-05-27 01:04:59 -0500

    404 handling

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -2,14 +2,14 @@ import React, { useEffect } from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 import "./App.css";
-import Home from "./components/Home.js";
-import About from "./components/About.js";
-import Resume from "./components/Resume.js";
-import Projects from "./components/Projects.js";
-import Error from "./components/Error.js";
-import Navigation from "./components/Navigation.js";
-import Unsubscribe from "./components/Unsubscribe.js";
-import Footer from "./components/Footer.js";
+import Home from "./pages/Home.js";
+import About from "./pages/About.js";
+import Resume from "./pages/Resume.js";
+import Projects from "./pages/Projects.js";
+import Error from "./pages/Error.js";
+import Navigation from "./pages/Navigation.js";
+import Unsubscribe from "./pages/Unsubscribe.js";
+import Footer from "./pages/Footer.js";
 import ScrollToTop from "./ScrollToTop.js";
 
 
---
+++ react-app/src/pages/Error.js
@@ -0,0 +1,26 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
+
+function Error() {
+    return (
+        <div className="min-h-[80vh] flex flex-col items-center justify-center">
+            <div className="text-7xl text-indigo-500 mb-4">
+                <span role="img" aria-label="confused face">😕</span>
+            </div>
+            <h2 className="text-2xl font-bold text-slate-100 mb-2">
+                404 - Page Not Found
+            </h2>
+            <p className="text-slate-200 mb-8">
+                Oops! The page you are looking for does not exist.
+            </p>
+            <Link to="/" class="group">
+                <button class=" border-2 border-white text-white h-16 rounded-full py-3 px-6 m-auto mt-32 group-hover:bg-white group-hover:text-slate-800 transition-all ease-out duration-300">
+                Click here!
+                </button>
+            </Link>
+        </div>
+    );
+}
+
+export default Error;
